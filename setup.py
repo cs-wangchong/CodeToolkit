b4aaad588f1eba3da5372c2f4cdb8b703ccea424
wingchwang <1147833662@qq.com>
2022-12-20 11:06:40 +0800

    fix bug about jar file not found

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python -O
 
 version = {
-    "__version__"     : '0.2.0',
+    "__version__"     : '0.2.1',
     "__title__"       : 'CodeToolkit',
     "__description__" : 'A toolkit for processing source code',
     "__license__"     : 'GNU General Public License, version 3.0',
@@ -49,7 +49,7 @@ setup(
                                 'codetoolkit': [
                                     'predicates.txt',
                                     'verb.txt',
-                                    'CodeAnalysis.jar'
+                                    'CodeAnalysis-1.0-SNAPSHOT.jar'
                                 ],
                                 'codetoolkit.spiral': ['data/*'],
                                 'codetoolkit.posse': ['corpus/*', 'dicts/*']
