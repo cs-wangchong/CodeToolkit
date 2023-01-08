ac78aabfeb21e26aedc05785790f1a447736db48
wingchwang <1147833662@qq.com>
2023-01-09 01:35:31 +0800

    call graph

--- codetoolkit/dependency_analyzer.py
+++ codetoolkit/dependency_analyzer.py
@@ -5,17 +5,20 @@ import json
 
 import jpype
 
-DEFAULT_JAR = str(Path(__file__).parent / "CodeAnalysis.jar")
+DEFAULT_JAR = str(Path(__file__).parent / "JavaAnalysis.jar")
 
 class DependencyAnalyzer:
     def __init__(self, jar_path=DEFAULT_JAR):
         jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", f"-Djava.class.path={jar_path}")
-        self.CFGBuilder = jpype.JClass('codeintelligence.codeanalysis.extractors.dependence.cfg.CFGBuilder')
-        self.PDGBuilder = jpype.JClass('codeintelligence.codeanalysis.extractors.dependence.pdg.PDGBuilder')
+        self.CFGBuilder = jpype.JClass('codetoolkit.janalysis.dg.cfg.CFGBuilder')
+        self.PDGBuilder = jpype.JClass('codetoolkit.janalysis.dg.pdg.PDGBuilder')
 
     def __del__(self):
-        if jpype.isJVMStarted():
-            jpype.shutdownJVM()
+        try:
+            if jpype.isJVMStarted():
+                jpype.shutdownJVM()
+        except Exception:
+            pass
 
     def build_cfg(self, code):
         cfgs = self.CFGBuilder.buildWithCode(code)
