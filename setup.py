e23b36bec37a6b2fc7903be7c7cb61ddbd854fa1
wingchwang <1147833662@qq.com>
2023-07-14 16:19:36 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python -O
 
 version = {
-    "__version__"     : '0.2.7',
+    "__version__"     : '0.2.8',
     "__title__"       : 'CodeToolkit',
     "__description__" : 'A toolkit for processing source code',
     "__license__"     : 'GNU General Public License, version 3.0',
